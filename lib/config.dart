c7c574e95a0ac0bb5f341218b139cb8dad2e5e39
InTECHT <88443508+InTECHT@users.noreply.github.com>
2021-10-18 11:10:27 +0500

    hassan

---
+++ lib/config.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const myGrey = Color(0xfff7f7f7);
+const myBlack = Color(0xff222222);
+const myRed = Color(0xffb22f32);
+const titleGreen = Color(0xff008060);
+const titleRed = Color(0xfff54337);
+const stockGreen = Color(0xff25a799);
