227edfe6d354dc4c1a12838804b781d7535e8683
InTECHT <88443508+InTECHT@users.noreply.github.com>
2021-12-22 12:51:28 +0500

    Hassan ~ App Changes 22-12-2021

--- lib/Screens/bottomNav.dart
+++ lib/Screens/bottomNav.dart
@@ -51,50 +51,54 @@ class _BottomNavState extends State<BottomNav> {
           )
         : Scaffold(
             body: pageDecider(),
-            bottomNavigationBar: BottomNavigationBar(
-              enableFeedback: true,
-              type: BottomNavigationBarType.fixed,
-              onTap: (index) {
-                if (currentPage == index && currentPage == 0) {
-                  Navigator.popUntil(
-                      globalContextHome, (route) => route.isFirst);
-                } else if (currentPage == index && currentPage == 1) {
-                  Navigator.popUntil(
-                      globalContextSearch, (route) => route.isFirst);
-                } else if (currentPage == index && currentPage == 2) {
-                  Navigator.popUntil(
-                      globalContextCategories, (route) => route.isFirst);
-                } else if (currentPage == index && currentPage == 3) {
-                  Navigator.popUntil(
-                      globalContextMyBag, (route) => route.isFirst);
-                } else if (currentPage == index && currentPage == 4) {
-                  Navigator.popUntil(
-                      globalContextProfile, (route) => route.isFirst);
-                } else {}
-                setState(() {
-                  currentPage = index;
-                  _pageController.animateToPage(currentPage,
-                      duration: const Duration(milliseconds: 600),
-                      curve: Curves.easeInOut);
-                });
-              },
-              currentIndex: currentPage,
-              showUnselectedLabels: true,
-              selectedItemColor: myWhite,
-              unselectedItemColor: myWhite,
-              unselectedLabelStyle:
-                  TextStyle(fontSize: dynamicWidth(context, 0.026)),
-              selectedLabelStyle:
-                  TextStyle(fontSize: dynamicWidth(context, 0.026)),
-              backgroundColor: myRed,
-              items: [
-                bottomItemModel("assets/icons/homeIcon.png", "Home"),
-                bottomItemModel("assets/icons/searchIcon.png", "Search"),
-                bottomItemModel("assets/icons/categoryIcon.png", "Categories"),
-                bottomItemModel("assets/icons/cartIcon.png", "My Bag",
-                    check: true),
-                bottomItemModel("assets/icons/profileIcon.png", "Profile")
-              ],
+            bottomNavigationBar: SizedBox(
+              height: dynamicHeight(context, .056),
+              child: BottomNavigationBar(
+                enableFeedback: true,
+                type: BottomNavigationBarType.fixed,
+                onTap: (index) {
+                  if (currentPage == index && currentPage == 0) {
+                    Navigator.popUntil(
+                        globalContextHome, (route) => route.isFirst);
+                  } else if (currentPage == index && currentPage == 1) {
+                    Navigator.popUntil(
+                        globalContextSearch, (route) => route.isFirst);
+                  } else if (currentPage == index && currentPage == 2) {
+                    Navigator.popUntil(
+                        globalContextCategories, (route) => route.isFirst);
+                  } else if (currentPage == index && currentPage == 3) {
+                    Navigator.popUntil(
+                        globalContextMyBag, (route) => route.isFirst);
+                  } else if (currentPage == index && currentPage == 4) {
+                    Navigator.popUntil(
+                        globalContextProfile, (route) => route.isFirst);
+                  } else {}
+                  setState(() {
+                    currentPage = index;
+                    _pageController.animateToPage(currentPage,
+                        duration: const Duration(milliseconds: 600),
+                        curve: Curves.easeInOut);
+                  });
+                },
+                currentIndex: currentPage,
+                showUnselectedLabels: true,
+                selectedItemColor: myWhite,
+                unselectedItemColor: myWhite,
+                unselectedLabelStyle:
+                    TextStyle(fontSize: dynamicWidth(context, 0.026)),
+                selectedLabelStyle:
+                    TextStyle(fontSize: dynamicWidth(context, 0.026)),
+                backgroundColor: myRed,
+                items: [
+                  bottomItemModel("assets/icons/homeIcon.png", "Home"),
+                  bottomItemModel("assets/icons/searchIcon.png", "Search"),
+                  bottomItemModel(
+                      "assets/icons/categoryIcon.png", "Categories"),
+                  bottomItemModel("assets/icons/cartIcon.png", "My Bag",
+                      check: true),
+                  bottomItemModel("assets/icons/profileIcon.png", "Profile")
+                ],
+              ),
             ),
           );
   }
@@ -108,7 +112,9 @@ class _BottomNavState extends State<BottomNav> {
                 badgeColor: myWhite,
                 badgeContent: Text(
                   cartItems.length.toString(),
-                  style: TextStyle(fontSize: dynamicWidth(context, 0.02)),
+                  style: TextStyle(
+                    fontSize: dynamicWidth(context, 0.02),
+                  ),
                 ),
                 showBadge: cartItems.length == 0 ? false : true,
                 child: Image.asset(
@@ -138,28 +144,35 @@ class _BottomNavState extends State<BottomNav> {
       children: [
         pageModel(Home()),
         pageModel(SearchPage()),
-        pageModel(CategoriesPage(
-          controller: _pageController,
-        )),
-        pageModel(Cart(
-          controller: _pageController,
-        )),
-        pageModel(Profile(
-          controller: _pageController,
-        ))
+        pageModel(
+          CategoriesPage(
+            controller: _pageController,
+          ),
+        ),
+        pageModel(
+          Cart(
+            controller: _pageController,
+          ),
+        ),
+        pageModel(
+          Profile(
+            controller: _pageController,
+          ),
+        ),
       ],
     );
   }
 
   pageModel(page) {
     return MaterialApp(
-        title: 'Monark',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: 'Open Sans',
-          primarySwatch: primaryColor,
-          brightness: Brightness.light,
-        ),
-        home: page);
+      title: 'Monark',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Open Sans',
+        primarySwatch: primaryColor,
+        brightness: Brightness.light,
+      ),
+      home: page,
+    );
   }
 }
